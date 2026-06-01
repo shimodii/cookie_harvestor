f38fd1dcac39911279958c6c5c217f4bf161d612
shimodi <am.shahmoradi93@gmail.com>
2026-06-01 10:15:05 +0330

    something has changed

--- main.go
+++ main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"fmt"
+	"encoding/json"
+	"log"
+	"net/http"
 
 	"divar_cookie_harvestor/utils"
+	"divar_cookie_harvestor/interface"
 )
 
 func main() {
@@ -16,5 +20,25 @@ func main() {
 
 	// sample for print a message with colors
 	// utils.Print("test message with color red", utils.Red)
+
+	// making request
+	req := interface.Make_phone_number_request(phone_num)
+	data, err := json.Marshal(req)
+	if err != nil {
+		utils.Print("failed to marshal request", utils.Red)
+		return
+	}
+
+	// sending request
+	resp, err := http.Post(utils.API_SENDING_PHONE_NUMBER, "application/json", bytes.NewBuffer(data))
+	if err != nil {
+		utils.Print("failed to send request", utils.Red)
+		log.Fatal(err)
+		return
+	}
+	defer resp.Body.Close()
+
+	// reading response
+
 
 }
