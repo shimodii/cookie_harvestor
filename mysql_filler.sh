f8cd970d5a629337defb699cb8c4f6f025787b35
shimodi <am.shahmoradi93@gmail.com>
2026-06-08 14:26:37 +0330

    updated db, updated filler

--- mysql_filler.sh
+++ mysql_filler.sh
@@ -13,19 +13,21 @@ cd $OUT_DIR
 
 for i in $(ls)
 do
-  access_token=$(cat $i | jq '.["sAccessToken"]')
-  front_token=$(cat $i | jq '.["sFrontToken"]')
-  parsed_access=$(echo $access_token | cut -c2- | rev | cut -c2- | rev)
-  parsed_front=$(echo $front_token | cut -c2- | rev | cut -c2- | rev)
-  file_name=$(echo $i | rev | cut -c5- | rev)
-  cookie="sAccessToken=$parsed_access; sFrontToken=$parsed_front"
-  echo "$cookie"
-  # echo "access token: $access_token"
-  # echo "front token: $front_token"
-  # echo "phone_number: $file_name"
+  access_token=$(cat $i | jq '.["sAccessToken"]' | rev | cut -c2- | rev | cut -c2-)
+  front_token=$(cat $i | jq '.["sFrontToken"]' | rev | cut -c2- | rev | cut -c2-)
+  refresh_token=$(cat $i | jq '.["sRefreshToken"]' | rev | cut -c2- | rev | cut -c2-)
+  # parsed_access=$(echo $access_token | cut -c2- | rev | cut -c2- | rev)
+  # parsed_front=$(echo $front_token | cut -c2- | rev | cut -c2- | rev)
+  phone_number=$(echo $i | rev | cut -c5- | rev)
+  # cookie="sAccessToken=$parsed_access; sFrontToken=$parsed_front"
+  # echo "$cookie"
+  echo "access token: $access_token"
+  echo "front token: $front_token"
+  echo "phone number: $phone_number"
+  echo "refresh token: $refresh_token"
 #  mysql -h "$DB_HOST" -u "$DB_USER" -p"$DB_PASS" "$DB_NAME" <<EOF
-#INSERT INTO divar_cookie (cookie, phone_number)
-#VALUES ('$cookie', '$file_name')
+#INSERT INTO divar_cookie (phone_number, refresh_token, access_token, front_token, updated_at, is_active)
+#VALUES ('$phone_number', '$refresh_token', '$access_token', '$front_token', $(date "+%s"), '1')
 #EOF
 done
 
